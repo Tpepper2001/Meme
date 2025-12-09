e072a009c305f396f17f74165ee7fe5569a52312
Tpepper2001 <oluwatoyinishola2024@gmail.com>
2025-12-09 09:05:09 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MemeEditor from './components/MemeEditor';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MemeEditor />
+    </div>
+  );
+}
+
+export default App;
